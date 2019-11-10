0a5681bc2b643bc5733426b47095b171c8388d66
evs179 <57573419+evs179@users.noreply.github.com>
2019-11-10 15:43:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-// INSERT JS HERE
 
-
-// SOCIAL PANEL JS
-const floating_btn = document.querySelector('.floating-btn');
-const close_btn = document.querySelector('.close-btn');
-
-floating_btn.addEventListener('click', () => {
-});
-
-close_btn.addEventListener('click', () => {
-});
