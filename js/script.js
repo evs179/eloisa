a024956d3cfafc700943a7e814614eb9602f13c3
evs179 <57573419+evs179@users.noreply.github.com>
2019-11-10 12:42:09 +0530

    3

--- js/script.js
+++
@@ -1,7 +0,0 @@
-$('.fixed.french').on('click', function(){
-  $('html, body').animate({scrollTop: $('#french-press').offset().top}, 1000);
-});
-
-$('.fixed.aero').on('click', function(){
-  $('html, body').animate({scrollTop: $('#aeropress').offset().top}, 1000);
-});
